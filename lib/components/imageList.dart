2662a500054f2441583e545eef37a8516a0c469b
Afeef K K <45afeef@gmail.com>
2022-11-01 09:47:29 +0530

    Added state to imageList

--- lib/components/imageList.dart
+++ lib/components/imageList.dart
@@ -10,30 +10,42 @@ class ImageList extends StatefulWidget {
 }
 
 class _ImageListState extends State<ImageList> {
+  int _selected = 0;
+
+  void _setSelected(int i) {
+    setState(() {
+      _selected = i;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Row(
       children: List.generate(
-          4,
-          (i) => Card(
-                margin: const EdgeInsets.all(0),
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(15)),
-                elevation: 3,
-                color: i % 3 == 1 ? Colors.white : const Color(0x77ffffff),
-                child: Padding(
-                  padding: const EdgeInsets.all(5.0),
-                  child: ClipRRect(
-                    borderRadius: const BorderRadius.all(Radius.circular(15)),
-                    child: Image.network(
-                      "https://images.unsplash.com/photo-1664575197229-3bbebc281874?q=40&w=60",
-                      width: i % 3 == 1 ? 70 : 60,
-                      height: i % 3 == 1 ? 70 : 60,
-                      fit: BoxFit.cover,
-                    ),
-                  ),
+        4,
+        (i) => GestureDetector(
+          onTap: (() => _setSelected(i)),
+          child: Card(
+            margin: const EdgeInsets.all(0),
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+            elevation: 3,
+            color: i == _selected ? Colors.white : const Color(0x77ffffff),
+            child: Padding(
+              padding: const EdgeInsets.all(5.0),
+              child: ClipRRect(
+                borderRadius: const BorderRadius.all(Radius.circular(15)),
+                child: Image.network(
+                  "https://images.unsplash.com/photo-1664575197229-3bbebc281874?q=40&w=60",
+                  width: i == _selected ? 70 : 60,
+                  height: i == _selected ? 70 : 60,
+                  fit: BoxFit.cover,
                 ),
-              )),
+              ),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
