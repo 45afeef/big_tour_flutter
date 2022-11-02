4497a5530525b7583d880a9fe4d166833504424d
Afeef K K <45afeef@gmail.com>
2022-11-02 08:01:25 +0530

    Updated the facilities widget with icons Added custom facility icons Installed flutter_svg package Added .vscode dir to gitignore

--- lib/components/facilities.dart
+++ lib/components/facilities.dart
@@ -1,32 +1,37 @@
 import 'dart:math';
-
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class Facilities extends StatelessWidget {
   const Facilities({Key? key, required this.size}) : super(key: key);
 
   final double size;
 
+  static List<String> s = [
+    "bonefire.svg",
+    "hiking.svg",
+    "hot_air_ballon.svg",
+    "kayak.svg",
+    "swimming.svg",
+    "tent.svg",
+  ];
+
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       height: size,
-      child: ListView(
-        scrollDirection: Axis.horizontal,
-        children: List.generate(
-            15,
-            (index) => SizedBox(
-                  width: size,
-                  child: Card(
-                    elevation: 6,
-                    child: Icon(
-                      size: min(size * 16 / 30, 24),
-                      Icons.accessible_forward_sharp,
-                      color: Colors.amber,
-                    ),
-                  ),
-                )),
-      ),
+      child: ListView(scrollDirection: Axis.horizontal, children: [
+        ...s.map((svgUrl) => SizedBox(
+              width: size,
+              child: Card(
+                  elevation: 6,
+                  child: SvgPicture.asset(
+                    'images/svg/$svgUrl',
+                    width: size,
+                    height: size,
+                  )),
+            ))
+      ]),
     );
   }
 }
